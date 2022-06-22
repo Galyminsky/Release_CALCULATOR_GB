727e58a94e8c30f1271a537ca9ffc0c863a548f3
Galyminsky-PC-Win <jobforandroid@protonmail.com>
2022-06-22 19:43:30 +0300

    Convert Java --> Kotlin

--- app/src/main/java/com/jobforandroid/myapplication/CalculatorModel.kt
+++ app/src/main/java/com/jobforandroid/myapplication/CalculatorModel.kt
@@ -1,148 +1,102 @@
-package com.jobforandroid.myapplication;
+package com.jobforandroid.myapplication
 
-public class CalculatorModel {
-    private int firstArg;
-    private int secondArg;
+class CalculatorModel {
+    private var firstArg = 0
+    private var secondArg = 0
+    private val inputStr = StringBuilder()
+    private var actionSelected = 0
+    private var state: State
 
-    private StringBuilder inputStr = new StringBuilder();
-
-    private int actionSelected;
-
-    private State state;
-
-    private enum State {
-        firstArgInput,
-        operationSelected,
-        secondArgInput,
-        resultShow
+    private enum class State {
+        firstArgInput, operationSelected, secondArgInput, resultShow
     }
 
-    public CalculatorModel() {
-        state = State.firstArgInput;
-    }
-
-    public void onNumPressed(int buttonId) {
-
+    fun onNumPressed(buttonId: Int) {
         if (state == State.resultShow) {
-            state = State.firstArgInput;
-            inputStr.setLength(0);
+            state = State.firstArgInput
+            inputStr.setLength(0)
         }
-
         if (state == State.operationSelected) {
-            state = State.secondArgInput;
-            inputStr.setLength(0);
+            state = State.secondArgInput
+            inputStr.setLength(0)
         }
-
-        if (inputStr.length() < 9) {
-            switch (buttonId) {
-                case R.id.zero:
-                    if (inputStr.length() != 0) {
-                        inputStr.append("0");
-                    }
-                    break;
-                case R.id.one:
-                    inputStr.append("1");
-                    break;
-                case R.id.two:
-                    inputStr.append("2");
-                    break;
-                case R.id.three:
-                    inputStr.append("3");
-                    break;
-                case R.id.four:
-                    inputStr.append("4");
-                    break;
-                case R.id.five:
-                    inputStr.append("5");
-                    break;
-                case R.id.six:
-                    inputStr.append("6");
-                    break;
-                case R.id.seven:
-                    inputStr.append("7");
-                    break;
-                case R.id.eight:
-                    inputStr.append("8");
-                    break;
-                case R.id.nine:
-                    inputStr.append("9");
-                    break;
+        if (inputStr.length < 9) {
+            when (buttonId) {
+                R.id.zero -> if (inputStr.length != 0) {
+                    inputStr.append("0")
+                }
+                R.id.one -> inputStr.append("1")
+                R.id.two -> inputStr.append("2")
+                R.id.three -> inputStr.append("3")
+                R.id.four -> inputStr.append("4")
+                R.id.five -> inputStr.append("5")
+                R.id.six -> inputStr.append("6")
+                R.id.seven -> inputStr.append("7")
+                R.id.eight -> inputStr.append("8")
+                R.id.nine -> inputStr.append("9")
             }
         }
-
     }
 
-    public void onActionPressed(int actionId) {
-        if (actionId == R.id.equals && state == State.secondArgInput && inputStr.length() > 0) {
-            secondArg = Integer.parseInt(inputStr.toString());
-            state = State.resultShow;
-            inputStr.setLength(0);
-            switch (actionSelected) {
-                case R.id.plus:
-                    inputStr.append(firstArg + secondArg);
-                    break;
-                case R.id.minus:
-                    inputStr.append(firstArg - secondArg);
-                    break;
-                case R.id.multiply:
-                    inputStr.append(firstArg * secondArg);
-                    break;
-                case R.id.division:
-                    inputStr.append(firstArg / secondArg);
-                    break;
+    fun onActionPressed(actionId: Int) {
+        if (actionId == R.id.equals && state == State.secondArgInput && inputStr.length > 0) {
+            secondArg = inputStr.toString().toInt()
+            state = State.resultShow
+            inputStr.setLength(0)
+            when (actionSelected) {
+                R.id.plus -> inputStr.append(firstArg + secondArg)
+                R.id.minus -> inputStr.append(firstArg - secondArg)
+                R.id.multiply -> inputStr.append(firstArg * secondArg)
+                R.id.division -> inputStr.append(firstArg / secondArg)
             }
-
-        } else if (inputStr.length() > 0 && state == State.firstArgInput && actionId != R.id.equals) {
-            firstArg = Integer.parseInt(inputStr.toString());
-            state = State.operationSelected;
-            actionSelected = actionId;
+        } else if (inputStr.length > 0 && state == State.firstArgInput && actionId != R.id.equals) {
+            firstArg = inputStr.toString().toInt()
+            state = State.operationSelected
+            actionSelected = actionId
         }
     }
 
-    public String getText() {
-        StringBuilder str = new StringBuilder();
-        switch (state) {
-            default:
-                return inputStr.toString();
-            case operationSelected:
-                return str.append(firstArg).append(' ')
-                        .append(getOperationChar())
-                        .toString();
-            case secondArgInput:
-                return str.append(firstArg).append(' ')
-                        .append(getOperationChar())
-                        .append(' ')
-                        .append(inputStr)
-                        .toString();
-            case resultShow:
-                return str.append(firstArg).append(' ')
-                        .append(getOperationChar())
-                        .append(' ')
-                        .append(secondArg)
-                        .append(" = ")
-                        .append(inputStr.toString())
-                        .toString();
+    val text: String
+        get() {
+            val str = StringBuilder()
+            return when (state) {
+                State.operationSelected -> str.append(
+                    firstArg
+                ).append(' ')
+                    .append(operationChar)
+                    .toString()
+                State.secondArgInput -> str.append(firstArg)
+                    .append(' ')
+                    .append(operationChar)
+                    .append(' ')
+                    .append(inputStr)
+                    .toString()
+                State.resultShow -> str.append(firstArg)
+                    .append(' ')
+                    .append(operationChar)
+                    .append(' ')
+                    .append(secondArg)
+                    .append(" = ")
+                    .append(inputStr.toString())
+                    .toString()
+                else -> inputStr.toString()
+            }
         }
+    private val operationChar: Char
+        private get() = when (actionSelected) {
+            R.id.plus -> '+'
+            R.id.minus -> '-'
+            R.id.multiply -> '*'
+            R.id.division -> '/'
+            else -> '/'
+        }
+
+    fun reset() {
+        state = State.firstArgInput
+        inputStr.setLength(0)
     }
 
-    private char getOperationChar() {
-        switch (actionSelected) {
-            case R.id.plus:
-                return '+';
-            case R.id.minus:
-                return '-';
-            case R.id.multiply:
-                return '*';
-            case R.id.division:
-            default:
-                return '/';
-
-        }
-    }
-
-    public void reset() {
-        state = State.firstArgInput;
-        inputStr.setLength(0);
+    init {
+        state = State.firstArgInput
     }
 }
-
--- app/src/main/java/com/jobforandroid/myapplication/MainActivity.kt
+++ app/src/main/java/com/jobforandroid/myapplication/MainActivity.kt
@@ -1,76 +1,54 @@
-package com.jobforandroid.myapplication;
+package com.jobforandroid.myapplication
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-
-public class MainActivity extends AppCompatActivity {
-
-    private CalculatorModel calculator;
-    private TextView text;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        int[] numberIds = new int[] {
-                R.id.zero,
-                R.id.one,
-                R.id.two,
-                R.id.three,
-                R.id.four,
-                R.id.five,
-                R.id.six,
-                R.id.seven,
-                R.id.eight,
-                R.id.nine
-        };
-
-        int[] actionsIds = new int[] {
-                R.id.plus,
-                R.id.minus,
-                R.id.multiply,
-                R.id.division,
-                R.id.equals
-        };
-
-        text = findViewById(R.id.text);
-
-        calculator = new CalculatorModel();
-
-        View.OnClickListener numberButtonClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                calculator.onNumPressed(view.getId());
-                text.setText(calculator.getText());
-            }
-        };
-
-        View.OnClickListener actionButtonOnclickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                calculator.onActionPressed(view.getId());
-                text.setText(calculator.getText());
-            }
-        };
-
-        for (int i = 0; i < numberIds.length; i++) {
-            findViewById(numberIds[i]).setOnClickListener(numberButtonClickListener);
+class MainActivity : AppCompatActivity() {
+    private var calculator: CalculatorModel? = null
+    private var text: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val numberIds = intArrayOf(
+            R.id.zero,
+            R.id.one,
+            R.id.two,
+            R.id.three,
+            R.id.four,
+            R.id.five,
+            R.id.six,
+            R.id.seven,
+            R.id.eight,
+            R.id.nine
+        )
+        val actionsIds = intArrayOf(
+            R.id.plus,
+            R.id.minus,
+            R.id.multiply,
+            R.id.division,
+            R.id.equals
+        )
+        text = findViewById(R.id.text)
+        calculator = CalculatorModel()
+        val numberButtonClickListener = View.OnClickListener { view ->
+            calculator!!.onNumPressed(view.id)
+            with(text) { this?.setText(calculator!!.text) }
         }
-
-        for (int i = 0; i < actionsIds.length; i++) {
-            findViewById(actionsIds[i]).setOnClickListener(actionButtonOnclickListener);
+        val actionButtonOnclickListener = View.OnClickListener { view ->
+            calculator!!.onActionPressed(view.id)
+            with(text) { this?.setText(calculator!!.text) }
         }
-
-        findViewById(R.id.reset).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                calculator.reset();
-                text.setText(calculator.getText());
-            }
-        });
+        for (i in numberIds.indices) {
+            findViewById<View>(numberIds[i]).setOnClickListener(numberButtonClickListener)
+        }
+        for (i in actionsIds.indices) {
+            findViewById<View>(actionsIds[i]).setOnClickListener(actionButtonOnclickListener)
+        }
+        findViewById<View>(R.id.reset).setOnClickListener {
+            calculator!!.reset()
+            with(text) { this?.setText(calculator!!.text) }
+        }
     }
-
 }
